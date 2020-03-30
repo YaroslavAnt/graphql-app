10a463a006e39050509630592d950fa8bcf77bed
YaroslavAnt <antonchyk.yar@gmail.com>
2020-03-31 01:10:59 +0300

    add-server-setup

---
+++ server/models/director.js
@@ -0,0 +1,9 @@
+﻿const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const directorSchema = new Schema({
+  name: String,
+  age: Number
+});
+
+module.exports = mongoose.model("Director", directorSchema);
---
+++ server/server/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("../schema/schema");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = 3005;
+
+mongoose.connect(
+  "mongodb+srv://YaroslavAnt:19860401@cluster0-1dbqr.mongodb.net/graphQL?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+const dbConnection = mongoose.connection;
+dbConnection.on("error", err => console.log("Error", err));
+dbConnection.once("open", () => console.log("Connected to db"));
+
+app.listen(PORT, err => {
+  err ? console.log(err) : console.log("Server started");
+});
