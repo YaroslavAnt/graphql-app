0f2a7ac071753474f33caa65255f6e71f5d96fe8
YaroslavAnt <antonchyk.yar@gmail.com>
2020-03-31 01:56:34 +0300

    add-components

--- server/server/app.js
+++ server/server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("../schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3005;
@@ -18,6 +19,8 @@ app.use(
     graphiql: true
   })
 );
+
+app.use(cors);
 
 const dbConnection = mongoose.connection;
 dbConnection.on("error", err => console.log("Error", err));
